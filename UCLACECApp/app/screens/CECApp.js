c9bed44c660dc0141d66fa7e427b11d83608d71c
Roslyn Lu <roslynlu@ucla.edu>
2018-02-13 18:49:23 -0800

    fetching successfully

--- UCLACECApp/app/screens/CECApp.js
+++ UCLACECApp/app/screens/CECApp.js
@@ -4,12 +4,13 @@ import {
   AsyncStorage,
   Text,
   View,
-  Button
+  Button,
+  ActivityIndicator, 
+  ListView
 } from 'react-native';
 import { connect } from "react-redux";
 import { addEvent } from '../actions/index.js';
 
-import { StackNavigator } from 'react-navigation';
 import Swiper from 'react-native-swiper';
 
 import Events from '../containers/events.js';
@@ -18,6 +19,35 @@ import Header from '../components/header.js';
 import data from '../../data.json'    // would be from backend
 
 class CECApp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    return fetch('http://52.53.224.70:3000/api/events.json')
+      .then(response => response.json())
+      .then(responseJson => {
+        let ds = new ListView.DataSource({
+          rowHasChanged: (r1, r2) => r1 !== r2,
+        });
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: ds.cloneWithRows(responseJson),
+          },
+          function() {
+            // do something with new state
+          }
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
   toggleDrawer = () => {
         this.props.navigator.toggleDrawer({
@@ -31,12 +61,26 @@ class CECApp extends Component {
   };
 
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
       <View style={{flex: 1}}>
         <Swiper
           showsButtons={false} loop={false} showsPagination={false} >
           <View style={{flex: 1}}> 
             <Header titleText="EVENTS" onClick={()=>this.toggleDrawer() }/>
+            <ListView
+              dataSource={this.state.dataSource}
+              renderRow={rowData => (
+                <Text>{rowData.title}, {rowData.event_type}</Text>
+              )}
+            />
             <Events
               data={data.events}
               addEvent={(event) => this.props.addEventClick(event)}
